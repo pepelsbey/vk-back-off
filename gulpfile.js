bc2dea35ee84e5ae6b18936fe5775d0059157108
Vadim Makeev <pepelsbey@gmail.com>
2018-06-24 02:27:39 +0300

    Fix indentation and included files

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,15 @@ const zip = require('gulp-zip');
 
 gulp.task('default', () => {
 	return gulp.src([
-			'_locales/**',
-			'images/**',
-			'background.js',
-			'manifest.json'
-		], {
-			base: '.'
-		})
-		.pipe(zip('vk-back-off.zip'))
-		.pipe(gulp.dest('.'))
-		.pipe(zip('vk-back-off.nex'))
-		.pipe(gulp.dest('.'));
+		'_locales/**',
+		'images/**',
+		'styles.css',
+		'manifest.json'
+	], {
+		base: '.'
+	})
+	.pipe(zip('vk-back-off.zip'))
+	.pipe(gulp.dest('.'))
+	.pipe(zip('vk-back-off.nex'))
+	.pipe(gulp.dest('.'));
 });
